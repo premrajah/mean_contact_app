b932ac6bedf0b67529356416c9fdf609cddf92e8
Prem Rajah <prajah@ctlondon.com>
2019-07-03 11:08:40 +0100

    Moved dummy data from controller to Server Setup get request with dummy data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,28 +1,17 @@
 var app = angular.module('myApp', []);
 
-
-app.controller('AppCtrl', function($scope) {
+app.controller('AppCtrl', function($scope, $http) {
   console.log("Running from controller");
 
-  person1 = {
-    name: "Prem Rajah",
-    email: "prem@rajah.com",
-    number: "07595 400 243"
-  }
-  person2 = {
-    name: "James Bond",
-    email: "james@bond.com",
-    number: "07595 123 456"
-  }
-  person3 = {
-    name: "John Smith",
-    email: "john@smith.com",
-    number: "07595 789 654"
-  }
+  // route to get data from
+  $http.get('/contactlist').then(function(response){
+      console.log("Received requested Data");
 
-  var contactList = [person1, person2, person3];
-  $scope.contactList = contactList;
+      $scope.contactlist = response.data;
+    });
 
-  console.log(contactList)
+  
+
+
 });
 
--- server.js
+++ server.js
@@ -8,5 +8,31 @@ var port = 3000;
 
 app.use(express.static(__dirname + "/public"));
 
+// GET REQUEST
+app.get('/contactlist', function(req, res){
+  console.log("Received a GET request");
+
+  // Dummy data
+  person1 = {
+    name: "Prem Rajah",
+    email: "prem@rajah.com",
+    number: "07595 400 243"
+  }
+  person2 = {
+    name: "James Bond",
+    email: "james@bond.com",
+    number: "07595 123 456"
+  }
+  person3 = {
+    name: "John Smith",
+    email: "john@smith.com",
+    number: "07595 789 654"
+  }
+
+  var contactList = [person1, person2, person3];
+  res.json(contactList); // responded by sending data as json
+
+});
+
 app.listen(port);
 console.log("Server On: " + port);
