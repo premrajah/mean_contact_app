228d93eaee1cd29162449f6da4377ec91d13df6a
Prem Rajah <prajah@ctlondon.com>
2019-07-03 12:33:33 +0100

    Got data from mongo db

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(express.static(__dirname + "/public"));
 app.get('/contactlist', function (req, res) {
   console.log("Received a GET request");
 
-  
+  db.contactlist.find(function(err, docs){
+    console.log(docs);
+    res.json(docs);
+  });
 
 });
 
