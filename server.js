790f07c634829108a599c50c0b718b9ab35c4dfb
Prem Rajah <prajah@ctlondon.com>
2019-07-03 12:31:10 +0100

    Removed dummy data

--- server.js
+++ server.js
@@ -15,25 +15,7 @@ app.use(express.static(__dirname + "/public"));
 app.get('/contactlist', function (req, res) {
   console.log("Received a GET request");
 
-  // Dummy data
-  person1 = {
-    name: "Prem Rajah",
-    email: "prem@rajah.com",
-    number: "07595 400 243"
-  }
-  person2 = {
-    name: "James Bond",
-    email: "james@bond.com",
-    number: "07595 123 456"
-  }
-  person3 = {
-    name: "John Smith",
-    email: "john@smith.com",
-    number: "07595 789 654"
-  }
-
-  var contactList = [person1, person2, person3];
-  res.json(contactList); // responded by sending data as json
+  
 
 });
 
