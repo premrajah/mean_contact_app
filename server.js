6ce90e7f31fb1d8e5148d35198740cfa8d554a58
Prem Rajah <prajah@ctlondon.com>
2019-07-03 12:30:39 +0100

    Added mongojs and basic setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var mongojs = require('mongojs');
+
 var app = express();
 var port = 3000;
+var db = mongojs('contactlist', ['contactlist']);
 
 // app.get('/', function(req, res){
 //   res.send("Hello World");
