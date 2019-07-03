d1f75e74df072c61de41022ff916d19e5b923462
Prem Rajah <prajah@ctlondon.com>
2019-07-03 14:44:09 +0100

    Added removce from db funcationality Added font-awesome 5

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,20 +3,42 @@ var app = angular.module('myApp', []);
 app.controller('AppCtrl', function ($scope, $http) {
   console.log("Running from controller");
 
-  // route to get data from
-  $http.get('/contactlist').then(function (response) {
-    console.log("Received requested GET Data");
+  // Function to refresh the page
+  var refresh = function () {
+    // route to get data from
+    $http.get('/contactlist').then(function (response) {
+      console.log("Received requested GET Data");
 
-    $scope.contactlist = response.data;
-  });
+      $scope.contactlist = response.data;
+    }).catch(function (err) {
+      console.log("An error occurred: " + err);
+    });
+  }
+  refresh();
 
-  $scope.addContact = function(){
+  $scope.addContact = function () {
     console.log($scope.contact);
 
-    $http.post('/contactlist', $scope.contact).then(function(response){
-      console.log("Received requested POST data");
-      console.log(response.data);
-    });
+    if ($scope.myForm.$valid) {
+      $http.post('/contactlist', $scope.contact).then(function (response) {
+        console.log("Received requested POST data");
+        console.log(response.data);
+        refresh();
+        $scope.contact = ''; // clear input boxes
+      }).catch(function (err) {
+        console.log("An error occurred: " + err);
+      });
+    }
+  }
+
+  $scope.remove = function (id) {
+    console.log(id);
+    $http.delete('/contactlist/' + id).then(function (response) {
+      console.log("Received requested DELETE data " + response.data.deletedCount);
+      refresh();
+    }).catch(function (err) {
+      console.log("An error occurred: " + err);
+    }); // send id to server api
   }
 
 
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/contactlist', function (req, res) {
   console.log("Received a GET request");
 
   db.contactlist.find(function(err, docs){
-    console.log(docs);
+    // console.log(docs);
     res.json(docs);
   });
 
@@ -26,12 +26,22 @@ app.get('/contactlist', function (req, res) {
 
 // POST REQUEST
 app.post('/contactlist', function(req, res) {
-  console.log("Received a POST request");
-  console.log(req.body);
+  console.log("Received a POST request: ");
+  // console.log(req.body);
 
   // Insert data into mongodb
   db.contactlist.insert(req.body, function(err, doc){
     res.json(doc); // send this to controller
+  });
+});
+
+app.delete('/contactlist/:id', function(req, res){
+  var id = req.params.id;  // get parameter id from url
+  console.log("Received a DELETE request: " + id);
+
+  // delete from db
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+    res.json(doc);
   });
 });
 
